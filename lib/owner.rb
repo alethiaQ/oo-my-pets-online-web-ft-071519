c0cd6ef3255951197df310485a30ba770b91df5e
Alethia Quintero <alethiaq22@gmail.com>
2019-07-30 14:08:13 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,31 @@
 class Owner
-  # code goes here
+
+  attr_reader :name, :species
+  @@all = []
+  def name(owner_name)
+    @name = owner_name
+    @@all << self
+  end
+  def initialize
+    @species = "human"
+  end
+
+  def say_species
+    puts "I am a #{species}"
+  end
+  def self.all
+    @@all
+  end
+  def self.count
+    self.all.count {|person| person}
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def buy_cat(name)
+    cat = Cat.new(name, self.name)
+  end
+
 end
