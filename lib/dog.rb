13a4e7d67c355bac2905647c7a14b029e3171edc
Alethia Quintero <alethiaq22@gmail.com>
2019-07-30 14:14:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog
-  # code goes here
+attr_reader :name
+attr_accessor :owner, :mood
+@@all = []
+
+def initialize(name, owner)
+  @name = name
+  @owner = owner
+  @mood = "mood"
+  @@all << self
+end
+
 end
